b637b20d7addbbae0eaa93eedb4d4e3990b880d2
ianAmpatin <141270505+ianAmpatin@users.noreply.github.com>
2023-08-07 03:56:21 +0800

    Delete Controller.java

--- Controller.java
+++
@@ -1,8 +0,0 @@
-public class Controller {
-    public static void main(String[] args) {
-        CashHolder client = new CashHolder("Client");
-        CashHolder owner = new CashHolder("Owner");
-
-        new GUI(client, owner);
-    }
-}
